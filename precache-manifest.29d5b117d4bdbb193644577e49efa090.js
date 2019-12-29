a16ba95f3d72f6623e02117a2341cbe95e71198c
AR123456 <coloradoz99@gmail.com>
2019-12-29 09:16:59 -0700

    Updates

---
+++ precache-manifest.29d5b117d4bdbb193644577e49efa090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e88276fa25d95c76f0337665470eabc",
+    "url": "/not-your-grammys-weather-app/index.html"
+  },
+  {
+    "revision": "7a82b2a03e3cb2ad1f22",
+    "url": "/not-your-grammys-weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7a82b2a03e3cb2ad1f22",
+    "url": "/not-your-grammys-weather-app/static/js/2.c03a8d5a.chunk.js"
+  },
+  {
+    "revision": "4ca4635d0d8efb357370",
+    "url": "/not-your-grammys-weather-app/static/js/main.26389589.chunk.js"
+  },
+  {
+    "revision": "496095bf4eaa701d243a",
+    "url": "/not-your-grammys-weather-app/static/js/runtime~main.72381c6d.js"
+  }
+]);
